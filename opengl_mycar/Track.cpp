05dbe910616cb323e4dee57140de2292e6d453be
sjnam <sjnam@sjnams-MacBook-Pro.local>
2016-12-02 19:04:22 +0900

    track 파일 분리

---
+++ opengl_mycar/Track.cpp
@@ -0,0 +1,9 @@
+//
+//  Track.cpp
+//  opengl_mycar
+//
+//  Created by sjnam on 12/2/16.
+//  Copyright © 2016 sjnam. All rights reserved.
+//
+
+#include "Track.hpp"
---
+++ opengl_mycar/Track.hpp
@@ -0,0 +1,14 @@
+//
+//  Track.hpp
+//  opengl_mycar
+//
+//  Created by sjnam on 12/2/16.
+//  Copyright © 2016 sjnam. All rights reserved.
+//
+
+#ifndef Track_hpp
+#define Track_hpp
+
+#include <stdio.h>
+
+#endif /* Track_hpp */
