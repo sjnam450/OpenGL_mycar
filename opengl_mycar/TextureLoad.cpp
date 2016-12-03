cd677d2e2cfb9412d6d182328905fd323564a3e7
sjnam <sjnam@sjnams-MacBook-Pro.local>
2016-12-04 01:53:46 +0900

    텍스처관련 클래스 추가 및 함수작성

---
+++ opengl_mycar/TextureLoad.cpp
@@ -0,0 +1,9 @@
+//
+//  TextureLoad.cpp
+//  opengl_mycar
+//
+//  Created by sjnam on 12/3/16.
+//  Copyright © 2016 sjnam. All rights reserved.
+//
+
+#include "TextureLoad.hpp"
---
+++ opengl_mycar/TextureLoad.hpp
@@ -0,0 +1,14 @@
+//
+//  TextureLoad.hpp
+//  opengl_mycar
+//
+//  Created by sjnam on 12/3/16.
+//  Copyright © 2016 sjnam. All rights reserved.
+//
+
+#ifndef TextureLoad_hpp
+#define TextureLoad_hpp
+
+#include <stdio.h>
+
+#endif /* TextureLoad_hpp */
