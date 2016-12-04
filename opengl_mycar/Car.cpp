27e2a6e226e688abb9b8fe16aa2a572f0a092b18
sjnam <sjnam@sjnams-MacBook-Pro.local>
2016-12-04 20:51:29 +0900

    경찰차 텍스처 입히기

--- opengl_mycar/Car.cpp
+++ opengl_mycar/Car.cpp
@@ -36,6 +36,7 @@ Car::Car() {
         TGAFILE tgaFile;
         texLoad.LoadTGAFile(objData->materialList[i]->texture_filename, &tgaFile);
         //tex[i] = i;
+        printf("tga file name : %s\n", objData->materialList[i]->texture_filename);
         memcpy(&tgaFiles[i], &tgaFile, sizeof(TGAFILE));
     }
     
@@ -48,10 +49,12 @@ Car::Car() {
         glBindTexture( GL_TEXTURE_2D, tex[i]);
         //GL_BGRA_EXT
         //GL_RG2
-        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, tgaFiles[i].imageWidth, tgaFiles[i].imageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, tgaFiles[i].imageData );
-        
         glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
         glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+
+        
+        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, tgaFiles[i].imageWidth, tgaFiles[i].imageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, tgaFiles[i].imageData );
+        
     }
     
 }
@@ -117,14 +120,41 @@ void Car::init_car() {
             //objData->faceList[i]->
             count++;
             //objData->materialList[objData->faceList[i]->material_index]-> ;
-            int texIndex0 = (GLuint)objData->faceList[i]->material_index+1;
+            int texIndex0 = (GLuint)objData->faceList[i]->material_index;
             
             glEnable(GL_TEXTURE_2D);
-            glBindTexture( GL_TEXTURE_2D, (GLuint)texIndex0 );
-            
+//            if (texIndex0 == -1 || texIndex0 == 0) {
+//                glBindTexture( GL_TEXTURE_2D, 2);
+//            }
+//            else if (texIndex0 == 1){
+//                glBindTexture( GL_TEXTURE_2D,  0);
+//            }
+//            else if(texIndex0 == 2) {
+//                glBindTexture( GL_TEXTURE_2D, 3);
+//            }
+            //glBindTexture( GL_TEXTURE_2D, 0);
+
+            if (texIndex0 == -1) {
+                glBindTexture( GL_TEXTURE_2D, 0);
+            }
+            else if (texIndex0 == 0){
+                glBindTexture(GL_TEXTURE_2D, 1 );
+            }
+
+            else if (texIndex0 == 1){
+                glBindTexture(GL_TEXTURE_2D, 2 );
+            }
+
+            else if (texIndex0 == 2){
+                glBindTexture(GL_TEXTURE_2D, 3 );
+            }
+            else {
+                int a;
+            }
+
             glBegin(GL_TRIANGLES);
-            printf("mat inde %d\n", objData->faceList[i]->material_index);
-//            objData->materialList[objData->faceList[i]->material_index]->texture_filename;
+            //printf("mat inde %d %s\n", objData->faceList[i]->material_index, objData->materialList[texIndex0]->texture_filename);
+            
 //            objData->materialList[objData->faceList[i]->material_index]->amb;
 //            objData->materialList[objData->faceList[i]->material_index]->diff;
 //            objData->materialList[objData->faceList[i]->material_index]->texture_filename;
@@ -134,6 +164,16 @@ void Car::init_car() {
             int vertexinde0 =  objData->faceList[i]->vertex_index[0];
             int vertexinde1 =  objData->faceList[i]->vertex_index[1];
             int vertexinde2 =  objData->faceList[i]->vertex_index[2];
+
+            int normInde0 = objData->faceList[i]->normal_index[0];
+            int normInde1 = objData->faceList[i]->normal_index[1];
+            int normInde2 = objData->faceList[i]->normal_index[2];
+
+            int textureInde0 = objData->faceList[i]->texture_index[0];
+            int textureInde1 = objData->faceList[i]->texture_index[1];
+            int textureInde2 = objData->faceList[i]->texture_index[2];
+            
+
             
             
             //int vertexinde3 =  objData->faceList[i]->vertex_index[3];
@@ -142,40 +182,60 @@ void Car::init_car() {
             //glBindTexture( GL_TEXTURE_2D, (GLuint)texIndex0 );
             //glBindTexture( GL_TEXTURE_2D, 1 );
             
-            GLfloat u1 = (GLfloat)objData->textureList[vertexinde0]->e[0];
-            GLfloat v1 = (GLfloat)objData->textureList[vertexinde0]->e[1];
+            if (textureInde0 != -1) {
+                
+            
+            GLfloat u1 = (GLfloat)objData->textureList[textureInde0]->e[0];
+            GLfloat v1 = (GLfloat)objData->textureList[textureInde0]->e[1];
             glTexCoord2f(u1, v1);
+                printf("uv %f, %f, texture in : %d\n", u1, v1, texIndex0);
+                
+
+            }
             glVertex3f((GLfloat)objData->vertexList[vertexinde0]->e[0], (GLfloat)objData->vertexList[vertexinde0]->e[1], (GLfloat)objData->vertexList[vertexinde0]->e[2]);
             glNormal3f((GLfloat)objData->normalList[vertexinde0]->e[0], (GLfloat)objData->normalList[vertexinde0]->e[1], (GLfloat)objData->normalList[vertexinde0]->e[2]);
             
-            printf("uv %f, %f, texture in : %d\n", u1, v1, texIndex0);
             
             
-            int texIndex1 = (GLuint)objData->faceList[i]->material_index +1;
             //glBindTexture( GL_TEXTURE_2D, (GLuint)texIndex1 );
-            GLfloat u2 = (GLfloat)objData->textureList[vertexinde1]->e[0];
-            GLfloat v2 = (GLfloat)objData->textureList[vertexinde1]->e[1];
-            glTexCoord2f(u2, v2);
+            
+            
+            if (textureInde1 != -1) {
+                
+                GLfloat u2 = (GLfloat)objData->textureList[textureInde1]->e[0];
+                GLfloat v2 = (GLfloat)objData->textureList[textureInde1]->e[1];
+                glTexCoord2f(u2, v2);
+                
+                
+            }
+            
+            
             glVertex3f((GLfloat)objData->vertexList[vertexinde1]->e[0], (GLfloat)objData->vertexList[vertexinde1]->e[1], (GLfloat)objData->vertexList[vertexinde1]->e[2]);
             glNormal3f((GLfloat)objData->normalList[vertexinde1]->e[0], (GLfloat)objData->normalList[vertexinde1]->e[1], (GLfloat)objData->normalList[vertexinde0]->e[2]);
+            //printf("uv %f, %f, texture in : %d\n", u1, v1, texIndex1);
+
             
 
             
-            int texIndex2 = (GLuint)objData->faceList[i]->material_index +1;
-            //glBindTexture( GL_TEXTURE_2D, (GLuint)texIndex2 );
-            GLfloat u3 = (GLfloat)objData->textureList[vertexinde2]->e[0];
-            GLfloat v3 = (GLfloat)objData->textureList[vertexinde2]->e[1];
-            glTexCoord2f(u3, v3);
+            
+            if (textureInde2 != -1) {
+                //glBindTexture( GL_TEXTURE_2D, (GLuint)texIndex2 );
+                GLfloat u3 = (GLfloat)objData->textureList[textureInde2]->e[0];
+                GLfloat v3 = (GLfloat)objData->textureList[textureInde2]->e[1];
+                glTexCoord2f(u3, v3);
+                
+                
+            }
+            
             glVertex3f((GLfloat)objData->vertexList[vertexinde2]->e[0], (GLfloat)objData->vertexList[vertexinde2]->e[1], (GLfloat)objData->vertexList[vertexinde2]->e[2]);
             glNormal3f((GLfloat)objData->normalList[vertexinde2]->e[0], (GLfloat)objData->normalList[vertexinde2]->e[1], (GLfloat)objData->normalList[vertexinde2]->e[2]);
+            //printf("uv %f, %f, texture in : %d\n", u1, v1, texIndex2);
         
+            
             glEnd();
+            glDisable(GL_TEXTURE_2D);
+            
         }
-        
-        glDisable(GL_TEXTURE_2D);
-        
-
-        
         printf("vertex cound : %d\n", count);
         
         
